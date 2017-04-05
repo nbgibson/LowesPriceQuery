011088bdee01722a243474053ca2580206ef4d60
WarriusBird <WarriusBird@users.noreply.github.com>
2017-04-05 09:41:31 -0400

    Uploaded script
    A repo isn't much use without code after all.

---
+++ lowesPriceCheck.py
@@ -0,0 +1,22 @@
+from lxml import html
+import requests
+import os
+
+print("Enter store number: ")
+storeNum = input()
+
+
+if storeNum.isdecimal():
+    shoppingListFile = open('list.txt')
+    shoppingListList = shoppingListFile.read().split(',')
+    cookies = dict(sn=storeNum)
+
+    for x in shoppingListList:
+        page = requests.get(r'https://www.lowes.com/search?searchTerm={}'.format(x), cookies=cookies)
+        tree = html.fromstring(page.content)
+
+        price = tree.xpath('//span[@class="primary-font jumbo strong"]/text()')
+        print('SKU: %s' % x, '\tPrice: %s' % price)
+
+else:
+    print("Please enter a valid store number. ie- ####")
